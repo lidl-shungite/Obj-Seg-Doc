28b355c6aaa5d7958f0643d8dc4fbf8665876a67
lidl-shungite <tutametro93@gmail.com>
2024-04-26 15:51:59 +0630

    update

--- pages/Documentation.py
+++ pages/Documentation.py
@@ -73,39 +73,30 @@ def main():
             ' used for segmentation of other things as well. Alas, U-Net has been chosen for our project. We will come back to why it is named \"U-Net\".'))
             container.divider()
             container.subheader('Data :blue[Searching] and :green[Collection]')
-            container.write_stream(stream_line('Realizing that PascalVOC dataset can be used in this case as well, we jumped straight to data processing part.'
-            ''))
+            container.write_stream(stream_line('I wanted to see how our custom trained model would perform on the dataset that I\'ve trained on YOLO. '
+                                               'So, it was decided that we would use PascalVOC for training U-Net.'))
             container.write_stream(stream_line('Check out the datasets on [Roboflow](https://public.roboflow.com/object-detection/pascal-voc-2012)' 
                                             ' and [Kaggle](https://www.kaggle.com/datasets/gopalbhattrai/pascal-voc-2012-dataset).'))
             container.divider()
             container.subheader('Data :blue[Understanding] and :green[Preprocessing]')
-            container.write_stream(stream_line(""))
-            container.write_stream(stream_line('Check out his video on [YouTube](https://www.youtube.com/watch?v=aVKGjzAUHz0)' 
-                                            ' and his [GitHub](https://github.com/computervisioneng/image-segmentation-yolov8) repository.'))
+            container.write_stream(stream_line("We wanted to make the U-Net model outputs binary mask, so that it would be much better off,"
+                                               " when comparing two models. So, I set out to process the mask images which are in black or "
+                                               "in some other different color, to be solely in black and white. This was easily achieved by "
+                                               "normalizing the pixel values of the mask image, by simply dividing it with :blue[255.0]. "
+                                               "This process made the pixel values to be between :blue[0] and :blue[1]. The values are then multiplied by :green[255] "
+                                               "to make the binary mask."))
+            container.write_stream(stream_line('Check out the referenced source code from this [YouTube](https://youtu.be/n4_ZuntLGjg?si=7zq-8cpIoaHgSDKN) video.'))
             container.divider()
-            container.subheader("Transfering YOLOv8 :blue[Model]")
-            container.write_stream(stream_line("Firstly, we install :blue[ultralytics] library that contains YOLO models."))
-            pip_in = '''pip install ultralytics'''
-            container.code(pip_in, language='python')
-            container.write_stream(stream_line("Next we import the library and download the yolo model by running the code below."))
-            ult_imp = '''
-        from ultralytics import YOLO
-
-        # Create a new YOLO model from scratch
-        model = YOLO('yolov8n-seg.yaml')
-
-        # Load a pretrained YOLO model (recommended for training)
-        model = YOLO('yolov8n-seg.pt')
-                        
-        '''
-            container.code(ult_imp, language='python')
-            container.write_stream(stream_line("After downloading a model, it's trained with modified Pascal VOC dataset by passing the :blue[config.yaml]"
-                                            " file that contains the total number of classes in the dataset along with their names and the paths to training and validation image folders."))
-            train_mod_c = '''
-            #Set and adjust epoch and imgsz as to your need
-            results = model.train(data='config.yaml', epochs=100, imgsz=640)''' 
-            container.code(train_mod_c, language='python')
-            container.write_stream(stream_line("The next step would be building an application out of this model."))
+            container.subheader("Understanding :blue[U-Net]")
+            container.image('images/unet_arch.png', caption='Architecture of U-Net')
+            container.write_stream(stream_line("U-Net is a Recurrent Neural Network architecture, which uses encoders and decoders."
+                                               " In the provided picture, blocks that are named \":blue[conv]\", are the encoders and their"
+                                               "purpose is to extract crucial features. The blocks named \":blue[deconv]\" are decoders, also known as \"UpSampling Layers\""
+                                               " which are used to rebuild the desampled down images. At the start of a segmentation process, the data is passed "
+                                               "along the encoding layers, down sampling them. :red[Notice]: _Copies of the down sampled values are send to the decoded"
+                                               "blocks to skip connections, where later they will be concatted upon the_"))
+            
+            
             container.divider()
             container.subheader("Comparison of Original Image and Output of YOLO")
             img_opt = container.selectbox("Select an option:",("A Single Cat","Cat & Human"))
