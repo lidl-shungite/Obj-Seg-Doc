3b1b8568f04b9460b9e20a500d22a3289611a2a0
lidl-shungite <159305657+lidl-shungite@users.noreply.github.com>
2024-05-06 18:41:59 +0630

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-import seaborn as sns
-import matplotlib.pyplot as plt
-import random
-
-df = pd.read_csv("logs/1st_logs.csv")
-new_df = df[["epoch","mean_squared_error","val_mean_squared_error"]]
-new_df.to_csv("logs/mse_log.csv", index=False)
